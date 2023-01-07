2c230e2ade8fb443b49ffa8a8460faaff60b18f4
mspronesti <massimiliano.pronesti@gmail.com>
2023-01-07 20:56:30 +0100

    feat(schedulers): add euler ancestral discrete scheduler implement euler ancestral discrete scheduler following HF implementation: https://github.com/huggingface/diffusers/blob/main/src/diffusers/schedulers/scheduling_euler_ancestral_discrete.py

---
+++ src/schedulers/euler_ancestral_discrete.rs
@@ -0,0 +1,139 @@
+use super::{interp, BetaSchedule, PredictionType};
+use tch::{kind, Kind, Tensor};
+
+#[derive(Debug, Clone)]
+pub struct EulerAncestralDiscreteSchedulerConfig {
+    /// The value of beta at the beginning of training.
+    pub beta_start: f64,
+    /// The value of beta at the end of training.
+    pub beta_end: f64,
+    /// How beta evolved during training.
+    pub beta_schedule: BetaSchedule,
+    /// number of diffusion steps used to train the model.
+    pub train_timesteps: usize,
+    /// prediction type of the scheduler function
+    pub prediction_type: PredictionType,
+}
+
+impl Default for EulerAncestralDiscreteSchedulerConfig {
+    fn default() -> Self {
+        Self {
+            beta_start: 0.0001,
+            beta_end: 0.02,
+            beta_schedule: BetaSchedule::Linear,
+            train_timesteps: 1000,
+            prediction_type: PredictionType::Epsilon,
+        }
+    }
+}
+
+/// Ancestral sampling with Euler method steps.
+/// Based on the original k-diffusion implementation by Katherine Crowson:
+///
+/// https://github.com/crowsonkb/k-diffusion/blob/481677d114f6ea445aa009cf5bd7a9cdee909e47/k_diffusion/sampling.py#L72
+#[derive(Clone)]
+pub struct EulerAncestralDiscreteScheduler {
+    timesteps: Vec<f64>,
+    sigmas: Vec<f64>,
+    init_noise_sigma: f64,
+    pub config: EulerAncestralDiscreteSchedulerConfig,
+}
+
+impl EulerAncestralDiscreteScheduler {
+    pub fn new(inference_steps: usize, config: EulerAncestralDiscreteSchedulerConfig) -> Self {
+        let betas = match config.beta_schedule {
+            BetaSchedule::ScaledLinear => Tensor::linspace(
+                config.beta_start.sqrt(),
+                config.beta_end.sqrt(),
+                config.train_timesteps as i64,
+                kind::FLOAT_CPU,
+            )
+            .square(),
+            BetaSchedule::Linear => Tensor::linspace(
+                config.beta_start,
+                config.beta_end,
+                config.train_timesteps as i64,
+                kind::FLOAT_CPU,
+            ),
+            _ => unimplemented!(
+                "EulerDiscreteScheduler only implements linear and scaled_linear betas."
+            ),
+        };
+
+        let alphas: Tensor = 1. - betas;
+        let alphas_cumprod = alphas.cumprod(0, Kind::Double);
+
+        // https://github.com/huggingface/diffusers/blob/2bd53a940c60d13421d9e8887af96b30a53c1b95/src/diffusers/schedulers/scheduling_euler_discrete.py#L149
+        let step = (config.train_timesteps - 1) as f64 / (inference_steps - 1) as f64;
+        let timesteps: Vec<f64> = (0..inference_steps).map(|i| i as f64 * step).rev().collect();
+
+        let sigmas = ((1. - &alphas_cumprod) as Tensor / &alphas_cumprod).sqrt();
+        let sigmas = interp(
+            &timesteps, // x-coordinates at which to evaluate the interpolated values
+            Tensor::range(0, sigmas.size1().unwrap() - 1, kind::FLOAT_CPU),
+            sigmas,
+        );
+
+        let mut sigmas = Vec::<f64>::from(sigmas);
+        sigmas.push(0.0);
+
+        // extracts max of sigmas, i.e. sigmas.max()
+        let init_noise_sigma = *sigmas.iter().max_by(|a, b| a.total_cmp(b)).unwrap();
+        Self { timesteps, sigmas, init_noise_sigma, config }
+    }
+
+    pub fn timesteps(&self) -> &[f64] {
+        self.timesteps.as_slice()
+    }
+
+    pub fn scale_model_input(&self, sample: Tensor, timestep: f64) -> Tensor {
+        let step_index = self.timesteps.iter().position(|&t| t == timestep).unwrap();
+        let sigma = self.sigmas[step_index];
+
+        // https://github.com/huggingface/diffusers/blob/aba2a65d6ab47c0d1c12fa47e9b238c1d3e34512/src/diffusers/schedulers/scheduling_euler_ancestral_discrete.py#L132
+        sample / (sigma.powi(2) + 1.).sqrt()
+    }
+
+    pub fn step(&self, model_output: &Tensor, timestep: f64, sample: &Tensor) -> Tensor {
+        let step_index = self.timesteps.iter().position(|&t| t == timestep).unwrap();
+        let sigma = self.sigmas[step_index];
+
+        // 1. compute predicted original sample (x_0) from sigma-scaled predicted noise
+        let pred_original_sample = match self.config.prediction_type {
+            PredictionType::Epsilon => sample - sigma * model_output,
+            PredictionType::VPrediction => {
+                model_output * (-sigma / (sigma.powi(2) + 1.).sqrt())
+                    + (sample / (sigma.powi(2) + 1.))
+            }
+            _ => unimplemented!("Prediction type must be one of `epsilon` or `v_prediction`"),
+        };
+
+        let sigma_from = self.sigmas[step_index];
+        let sigma_to = self.sigmas[step_index + 1];
+        let sigma_up = (sigma_to.powi(2) * (sigma_from.powi(2) - sigma_to.powi(2))
+            / sigma_from.powi(2))
+        .sqrt();
+        let sigma_down = (sigma_to.powi(2) - sigma_up.powi(2)).sqrt();
+
+        // 2. Convert to an ODE derivative
+        let derivative = (sample - pred_original_sample) / sigma;
+        let dt = sigma_down - sigma;
+
+        let prev_sample = sample + derivative * dt;
+        let noise = Tensor::randn_like(model_output);
+
+        prev_sample + noise * sigma_up
+    }
+
+    pub fn init_noise_sigma(&self) -> f64 {
+        self.init_noise_sigma
+    }
+
+    pub fn add_noise(&self, original_samples: &Tensor, noise: Tensor, timestep: f64) -> Tensor {
+        let step_index = self.timesteps.iter().position(|&t| t == timestep).unwrap();
+        let sigma = self.sigmas[step_index];
+
+        // noisy samples
+        original_samples + noise * sigma
+    }
+}
