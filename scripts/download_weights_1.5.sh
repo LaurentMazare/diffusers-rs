5238f28b31f8aab44270c5755beea09ac9910308
oriont <elijahotarr@gmail.com>
2023-05-24 19:49:15 -0700

    files shouldn't have been changed

--- scripts/download_weights_1.5.sh
+++ scripts/download_weights_1.5.sh
@@ -28,16 +28,16 @@ wget_vae_unet_weights() {
   header="Authorization: Bearer $1"
   wget --header="$header" https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/$BRANCH/vae/diffusion_pytorch_model.bin -O vae.bin
   # download weights for unet
-  wget --header="$header" https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/$BRANCH/unet/diffusion_pytorch_model.bin -O unet.bin
-
+  wget --header="$header" https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/$BRANCH/unet/diffusion_pytorch_model.bin -O unet.bin	
+  
   # convert to npz
   LD_LIBRARY_PATH= python3 -c "
 import torch
 from safetensors.torch import save_file
-
+  
 model = torch.load('./vae.bin')
 save_file(dict(model), './vae.safetensors')
-
+  
 model = torch.load('./unet.bin')
 save_file(dict(model), './unet.safetensors')
 "
@@ -55,9 +55,9 @@ cd data
 
 echo "Getting the Weights and the Vocab File"
 # get the weights
-wget_vocab
-wget_clip_weights
-wget_vae_unet_weights $1
+wget_vocab 
+wget_clip_weights 
+wget_vae_unet_weights $1 
 
 echo "Cleaning ..."
 rm -rf $ROOT/data/*.bin
