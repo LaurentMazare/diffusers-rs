21f0683ba8bc28fed02bf39ad3793fb4e1fe2445
oriont <elijahotarr@gmail.com>
2023-05-25 18:05:34 -0700

    Remove use_cpu option and fail on ensure_data_dir instead of deleting files

--- scripts/get_weights.py
+++ scripts/get_weights.py
@@ -6,35 +6,27 @@ import sys
 import argparse
 
 data_path = os.path.join(os.path.dirname(__file__), "../data/")
+vocab_filename = "bpe_simple_vocab_16e6.txt"
 
-def ensure_data_dir():
+def ensure_data_dir(safetensors):
     print("Ensuring empty data directory...")
-    if not os.path.exists(data_path):
-        os.mkdir(data_path)
-        print("Created empty data directory")
-        return
-    else:
-        print("Found data directory")
 
-    if os.listdir(data_path):
-        while (response := input("Data directory isn't empty. Would you like to overwrite its contents? [(y)/n]")) not in ["y","n",""]:
-            pass
-
-        if response == "n":
-            print("Aborting.")
+    if os.path.exists(data_path):
+        # Fail if conflicting files exist
+        files = os.listdir(data_path)
+        newfiles = [x for name in safetensors for x in (f"{name}.bin", f"{name}.safetensors")]
+        newfiles += [vocab_filename, f"{vocab_filename}.gz"]
+        conflicts = set(files) & set(newfiles)
+        if len(conflicts) != 0:
+            print("Error: please remove the following files from data directory:")
+            print(conflicts)
             sys.exit()
-        elif response == "y" or response == "":
-            print("Removing existing files...")
-            for i in os.listdir(data_path):
-                try:
-                    os.remove(os.path.join(data_path, i))
-                except:
-                    pass
-            return
+    else:
+        os.mkdir(data_path)
 
-    print("Data directory is empty!")
+    print("Found no conflicts!")
 
-def get_safetensors(safetensors, weight_bits, use_cpu):
+def get_safetensors(safetensors, weight_bits):
     for name, url in safetensors.items():
         print(f"Getting {name} {weight_bits} bit tensors...")
 
@@ -42,7 +34,7 @@ def get_safetensors(safetensors, weight_bits, use_cpu):
         urllib.request.urlretrieve(url, os.path.join(data_path, f"{name}.bin"))
 
         # Make safetensors file
-        model = torch.load(os.path.join(data_path, f"{name}.bin"), map_location=torch.device("cpu") if use_cpu else None)
+        model = torch.load(os.path.join(data_path, f"{name}.bin"), map_location=torch.device("cpu"))
         tensors = {k: v.clone().detach() for k, v in model.items() if 'text_model' in k} if name in ["clip_v2.1", "pytorch_model"] else dict(model)
         save_file(tensors, os.path.join(data_path, f"{name}.safetensors"))
 
@@ -51,12 +43,12 @@ def get_safetensors(safetensors, weight_bits, use_cpu):
 
 def get_vocab(vocab_url):
     print("Getting vocab...")
-    urllib.request.urlretrieve(vocab_url, os.path.join(data_path, "bpe_simple_vocab_16e6.txt.gz"))
+    urllib.request.urlretrieve(vocab_url, os.path.join(data_path, f"{vocab_filename}.gz"))
     import gzip
-    with gzip.open(os.path.join(data_path, "bpe_simple_vocab_16e6.txt.gz"), 'rb') as g:
-        with open(os.path.join(data_path, "bpe_simple_vocab_16e6.txt"), "xb") as f:
+    with gzip.open(os.path.join(data_path, f"{vocab_filename}.gz"), 'rb') as g:
+        with open(os.path.join(data_path, vocab_filename), "xb") as f:
             f.write(g.read())
-    os.remove(os.path.join(data_path, "bpe_simple_vocab_16e6.txt.gz"))
+    os.remove(os.path.join(data_path, f"{vocab_filename}.gz"))
 
 def get_urls(sd_version, weight_bits):
     branch = "main"
@@ -81,14 +73,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Download weights for diffusers-rs.")
     parser.add_argument("--sd_version", "-v", choices=["2.1", "1.5"], default="2.1")
     parser.add_argument("--weight_bits", "-w", choices=["16", "32"], default="16")
-    parser.add_argument("--use_cpu", "-c", action="store_true", default=False)
     args = parser.parse_args()
 
     print("Setting up model weights for diffusers-rs...")
 
     safetensors, vocab_url = get_urls(args.sd_version, args.weight_bits)
-    ensure_data_dir()
+    ensure_data_dir(safetensors)
     get_vocab(vocab_url)
-    get_safetensors(safetensors, args.weight_bits, args.use_cpu)
+    get_safetensors(safetensors, args.weight_bits)
 
     print("Finished!")
